322d5c73b3c3186dd875e301bbefb8e1a036f524
dragoon2014 <dragoon2014@users.noreply.github.com>
2022-12-17 18:11:18 +0900

    DX_NON_SOUNDが定義されていてもサンプルがビルドできるように修正

--- samples/sample05_sound.cpp
+++ samples/sample05_sound.cpp
@@ -7,8 +7,12 @@ int main(){
     if(DxLib_Init() == -1){
         return -1;
     }
+#ifdef DX_NON_SOUND
+    DrawString(0, 0, "DX_NON_SOUND is defined, do nothing", 0xffffff);
+#else
     DrawString(0, 0, "Playing sound...", 0xffffff);
     PlaySoundFile("assets/noise.ogg", DX_PLAYTYPE_LOOP);
+#endif
     WaitKey();
     DxLib_End();
     return 0;
